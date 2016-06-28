5ff7a56e4a35ee2e9b95fcc325fc1f4382d536df
Pawel Kruszewski <kruszewsky@gmail.com>
2016-06-28 11:08:49 +0200

    added script show file content

---
+++ show_file_content.groovy
@@ -0,0 +1,9 @@
+import com.liferay.portal.kernel.util.HtmlUtil
+
+try {
+   file = new File(System.getProperty('liferay.home'),'/jboss-eap-6.3/standalone/configuration/standalone.xml')
+   content = com.liferay.portal.kernel.util.FileUtil.read(file)
+   out.println(HtmlUtil.escape(content))
+} catch(Exception e) {
+  e.printStackTrace(out)
+}
