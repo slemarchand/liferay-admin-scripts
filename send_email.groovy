a23de49480f7e79b5a9f82b188b5a9cc9ca0e849
Pawel Kruszewski <kruszewsky@gmail.com>
2016-06-28 11:10:23 +0200

    added script to send email with MailEngine

---
+++ send_email.groovy
@@ -0,0 +1,14 @@
+import com.liferay.util.mail.*
+
+String from = ""
+String to = ""
+String subject="This is email title"
+String body="Hello World, this is my test email"
+
+try {
+  out.println("Before Mail Engine")
+  MailEngine.send(from, to, subject, body)
+  out.println("After Mail Engine")
+} catch(Exception e) {
+  e.printStackTrace(out)
+}
