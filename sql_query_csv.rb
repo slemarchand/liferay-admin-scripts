5e995caaa254382019e37b939af8d424fdac2cb3
Sébastien Le Marchand <slemarchand@users.noreply.github.com>
2017-01-05 17:34:42 +0100

    Update copyright

--- sql_query_csv.rb
+++ sql_query_csv.rb
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2012 Sébastien Le Marchand, All rights reserved.
+# Copyright (c) 2012-present Sébastien Le Marchand, All rights reserved.
 #
 # This library is free software; you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
